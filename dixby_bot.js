988fb4b2c9a06440be57d2c02a7ff5e84383042a
Michelle Hartley <mdhcodes@gmail.com>
2017-03-29 09:36:25 -0400

    Add program files

---
+++ dixby_bot.js
@@ -0,0 +1,2 @@
+// Get the data from keys.js and store the data in a variable named keys.
+var keys = require('keys.js');
--- keys.js
+++ keys.js
@@ -1,1 +1,8 @@
+console.log('this is loaded');
 
+exports.twitterKeys = {
+  consumer_key: 'VaPYtF72jsIbKGtVLvGOTsSyN',
+  consumer_secret: 'EZ02pYUbrdNeG8kqUeDXcz3N3V70UQVEI01ZRLFYn8Rs1B9J2h',
+  access_token_key: '4077768209-D8YawRDUXNGWj1qbGo0okStGW7GiPsf82fyNTW1',
+  access_token_secret: 'i0rJ7hmvJZbfqARtUP9SqRzr2N2SPBsxg5CF50SUHjEZC',
+}
